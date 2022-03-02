7522b9f87fec02291fe92e41fc8c8f5173b2fafd
Sh4rkD <20006132@es.uveg.edu.mx>
2022-03-02 09:05:36 -0700

    video 15 platzi, diseñando cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/*/.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        'sanFrancisco':"url('../img/sanFrancisco.jpg')",
-        'sanFranciscoDesktop':"url('../img/sanFrancisoDestop.jpg')",
-        'yosemite':"url('../img/yosemite.jpg')",
-        'LA':"url('../img/LA.jpg')",
-        'seattle':"url('../img/seattle.jpg')",
-        'new_york':"url('../img/new_york.jpg')",
-        'norway':"url('../img/norway.jpg')",
-        'sydney':"url('../img/sydney.jpg')",
-        'miami':"url('../img/miami.jpg')",
-        'switzerland':"url('../img/switzerland.jpg')",
-        'bali':"url('../img/bali.jpg')",
-        'chicago':"url('../img/chicago.jpg')",
-        'europe':"url('../img/europe.jpg')",
-        'iceland':"url('../img/iceland.jpg')"
-      }
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'new_york': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'bali': "url('../img/bali.jpg')",
+        'chicago': "url('../img/chicago.jpg')",
+        'europe': "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')"
+        },
+        backgroundColor: theme =>({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9'
+        }),
+        textColor:{
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9'
+        },
+        fontFamily:{
+          Montserrat:['Montserrat','sans-serif']
+        }
     },
-    backgroundColor: theme => ({
-      ...theme('colors'),
-      'primary':'#CC2D4A',
-      'secondary':'#8FA206',
-      'tertiary':'#61AEC9'
-    }),
-    textColor:{      
-      'primary':'#CC2D4A',
-      'secondary':'#8FA206',
-      'tertiary':'#61AEC9'
-    },
-    fontFamily:{
-      Montserrat:['Montserrat','sans-serif']
-    }
   },
   plugins: [
     require('@tailwindcss/forms'),
